9483937630ec4d2e09c70875d71e0e23a63fe900
mirai <nakajimami11@gmail.com>
2023-02-02 14:08:50 +0900

    Exam01Controllerにinputメソッド追加

--- src/main/java/com/example/controller/Exam1Controller.java
+++ src/main/java/com/example/controller/Exam1Controller.java
@@ -1,6 +1,7 @@
 package com.example.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,10 @@ public class Exam1Controller {
 	public String index() {
 		return "exam01";
 	}
-	
 
+	@PostMapping("/input")
+	public String input(String name, Model model) {
+		model.addAttribute(name);
+		return "exam01-result";
+	}
 }
